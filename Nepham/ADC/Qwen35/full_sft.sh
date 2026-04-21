bc0f01b648b8b34f79ae692efa788f53638e36c1
Nephamaster <1723433721@qq.com>
2026-04-21 17:30:04 +0800

    update

--- Nepham/ADC/Qwen35/full_sft.sh
+++ Nepham/ADC/Qwen35/full_sft.sh
@@ -1,11 +1,12 @@
-nproc_per_node=8
+nproc_per_node=6
 
 PYTORCH_CUDA_ALLOC_CONF='expandable_segments:True' \
 NPROC_PER_NODE=$nproc_per_node \
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 \
 swift sft \
     --output_dir Nepham/output/Adapter/Qwen35 \
-    --model /share/project/wuhaiming/data/models/Qwen3.5-9B-Base \
+    --model /share/project/wuhaiming/spaces/ms-swift/Nepham/output/Adapter/Qwen35/extract_from_v17 \
+    --model_type qwen \
     --check_model false \
     --custom_dataset_info Nepham/dataset_info.json \
     --dataset 34m_confuse_gen \
@@ -26,6 +27,8 @@ swift sft \
     --logging_steps 10 \
     --max_length 2048 \
     --warmup_ratio 0.05 \
-    --load_from_cache_file true \
+    --load_from_cache_file true
+    # --lr_scheduler_type constant \
+    # --tokenizer /share/project/wuhaiming/data/models/Qwen3.5-9B-Base \
     # --attn_impl flash_attention_2 \
     # --gradient_accumulation_steps $(expr 16 / $nproc_per_node) \
