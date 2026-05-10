4f50d2c60746aece93b5c97a7aab92e81a323162
Nephamaster <1723433721@qq.com>
2026-05-10 21:04:03 +0800

    update adc/qwen35/lora

--- Nepham/ADC/Qwen35/lora.sh
+++ Nepham/ADC/Qwen35/lora.sh
@@ -1,10 +1,10 @@
-nproc_per_node=1
+nproc_per_node=4
 
 PYTORCH_CUDA_ALLOC_CONF='expandable_segments:True' \
 NPROC_PER_NODE=$nproc_per_node \
-CUDA_VISIBLE_DEVICES=1 \
+CUDA_VISIBLE_DEVICES=4,5,6,7 \
 swift sft \
-    --output_dir Nepham/output/ADC/Qwen35/ \
+    --output_dir Nepham/output/Adapter/Qwen35-Char/ \
     --model ../ADC/charize/Qwen3.5-9B-Base-Char \
     --check_model false \
     --custom_dataset_info Nepham/dataset_info.json \
@@ -18,14 +18,14 @@ swift sft \
     --loss_scale ignore_empty_think \
     --torch_dtype bfloat16 \
     --tuner_type lora \
-    --lora_rank 8 \
+    --lora_rank 16 \
     --lora_alpha 32 \
     --target_modules all-linear \
     --deepspeed zero2 \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 32 \
+    --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 2 \
     --learning_rate 1e-4 \
     --eval_steps 500 \
     --save_steps 500 \
