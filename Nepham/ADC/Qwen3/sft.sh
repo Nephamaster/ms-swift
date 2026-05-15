7af52da670e5c58a1a10010960fedf9abf9cc69a
Nephamaster <1723433721@qq.com>
2026-05-15 11:53:04 +0800

    update qwen

--- Nepham/ADC/Qwen3/sft.sh
+++ Nepham/ADC/Qwen3/sft.sh
@@ -4,13 +4,15 @@ PYTORCH_CUDA_ALLOC_CONF='expandable_segments:True' \
 NPROC_PER_NODE=$nproc_per_node \
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
 swift sft \
-    --output_dir Nepham/output/ADC/Qwen35/ \
-    --model ../../data/models/Qwen3.5-9B-Base \
-    --model_type qwen3_5 \
+    --output_dir Nepham/output/ADC/Qwen3/ \
+    --model ../ADC/charize/Qwen3-8B-Base-Char \
+    --model_type qwen3 \
+    --template qwen3 \
     --check_model false \
     --custom_dataset_info Nepham/dataset_info.json \
     --dataset csc_train \
     --system Nepham/system_prompt/csc.txt \
+    --deepspeed zero2 \
     --dataloader_num_workers 32 \
     --dataset_num_proc 64 \
     --load_from_cache_file true \
@@ -18,27 +20,18 @@ swift sft \
     --add_non_thinking_prefix true \
     --loss_scale ignore_empty_think \
     --torch_dtype bfloat16 \
-    --tuner_type lora \
-    --lora_rank 16 \
-    --lora_alpha 32 \
-    --target_modules all-linear \
-    --deepspeed zero2 \
+    --tuner_type full \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 2 \
     --learning_rate 1e-4 \
-    --eval_steps 100 \
-    --save_steps 100 \
-    --save_total_limit 8 \
+    --eval_steps 250 \
+    --save_steps 250 \
+    --save_total_limit 5 \
     --logging_steps 10 \
     --max_length 2048 \
     --warmup_ratio 0.02
     
     # --model ../../data/models/Qwen3.5-9B-Base \
     # --model ../ADC/charize/Qwen3.5-9B-Base-Char \
-    # --group_by_length true \
-    # --lr_scheduler_type constant \
-    # --tokenizer /share/project/wuhaiming/data/models/Qwen3.5-9B-Base \
-    # --attn_impl flash_attention_2 \
-    # --gradient_accumulation_steps $(expr 16 / $nproc_per_node) \
--- Nepham/ADC/Qwen35/merge_lora.sh
+++ Nepham/ADC/Qwen35/merge_lora.sh
@@ -1,5 +1,5 @@
 swift export \
-    --adapters Nepham/output/ADC/Qwen35-Char/v4-20260513-002232/checkpoint-400/ \
-    --output_dir Nepham/output/ADC/Qwen35-Char/test2 \
+    --adapters Nepham/output/ADC/Qwen35/v0-20260513-015255/checkpoint-2496 \
+    --output_dir Nepham/output/ADC/Qwen35/test1 \
     --merge_lora true \
-    --max_shard_size 20GB
+    --max_shard_size 5GB
